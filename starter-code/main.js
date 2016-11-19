6832e24709293001909a820fd5c344eb6861cd23
Andrew Dallas <andrewdallas@Andrews-MacBook-Pro.local>
2016-11-18 21:55:27 -0500

    create bord w/js

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,7 +5,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
+/*if (cardTwo === cardFour){
 	alert("You found a match!")
 }else if (cardOne === cardTwo){
 	alert("You found a match!")
@@ -13,4 +13,14 @@ if (cardTwo === cardFour){
 	alert("You found a match!")
 }else {
 	alert("Sorry, try again.")
+}*/
+
+var board = document.getElementById('game-board');
+var createBoard = function () {
+    for (var i=0; i<4; i++) {
+        var newCardElement = document.createElement('div');
+        newCardElement.className = 'card';
+        board.appendChild(newCardElement);
+    }
 }
+createBoard();
